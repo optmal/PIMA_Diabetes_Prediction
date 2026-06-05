d627a12df6f4ea0c06b7c77502ff1e4d8ae25835
Fred Premji <105253295+optmal@users.noreply.github.com>
2026-06-05 13:05:29 -0400

    Change Hugging Face API token in hosting.py
    Updated the API token for Hugging Face in hosting.py.

--- self_paced_courses_1_mlops/hosting/hosting.py
+++ self_paced_courses_1_mlops/hosting/hosting.py
@@ -1,7 +1,7 @@
 from huggingface_hub import HfApi
 import os
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api = HfApi(token=os.getenv("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"))
 api.upload_folder(
     folder_path="self_paced_courses_1_mlops/deployment",
     repo_id="opt456758/PIMA-Diabetes-Prediction"                                         # enter the Hugging Face username here
